413ec13b95ad71a3458ccf62133e6f1916f00033
AnDrEwFuRsOvV <andreyfursov30977@gmail.com>
2023-06-01 16:44:57 +0300

    add some layout for top and header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
-$(function () {
+$(document).ready(function () {
+  // Toggle dropdown visibility and update selected item
+  $(".header__lang").click(function () {
+    $(".header__lang-list").toggleClass("open");
+    $(".header__selected-img").toggleClass("rotate");
+  });
 
-})
+  // Close dropdown and update selected item
+  $(".header__lang-item").click(function () {
+    var selectedItem = $(this).text();
+    $(".header__selected-item").text(selectedItem);
+    $(".header__lang-list").removeClass("open");
+    $(".header__selected-img").removeClass("rotate");
+  });
+});
